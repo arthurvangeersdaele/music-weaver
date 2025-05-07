72929f3ba6d361094b9d3193a08fcedd466c91b4
Arthur Van Geersdaele <64006823+arthurvangeersdaele@users.noreply.github.com>
2025-05-07 08:11:52 +0200

    Create sandbox.py

---
+++ sandbox.py
@@ -0,0 +1,15 @@
+## do your dummy testes here
+
+# 1 - mics and audio devices detection 
+
+# 2 - mic and audio device control
+
+# 3 - raw signal real time rendering
+
+# 4 - raw signal real time processing 
+
+# 5 - features real time extraction
+
+# 6 - feature-to-visuals functions
+
+
